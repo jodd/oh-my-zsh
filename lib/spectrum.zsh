dcaf241bc6499b1a282891d16296bba248a5599a
Joris Durand <joris.durand@free.fr>
2014-11-04 13:02:59 +0100

    function spectrum_ls() didnt work for me

--- lib/spectrum.zsh
+++ lib/spectrum.zsh
@@ -25,7 +25,8 @@ ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab o
 # Show all 256 colors with color number
 function spectrum_ls() {
   for code in {000..255}; do
-    print -P -- "$code: %F{$code}$ZSH_SPECTRUM_TEXT%f"
+    # print -P -- "$code: %F{$code}$ZSH_SPECTRUM_TEXT%f"
+    print -P -- "$code: %{$FG[$code]%}$ZSH_SPECTRUM_TEXT%f"
   done
 }
 
