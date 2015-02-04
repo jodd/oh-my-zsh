51cab8d8f99bf5b36a4bff94def7fcf2f4a42c06
Joris Durand <joris.durand@free.fr>
2015-02-04 12:19:29 +0100

    up alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,6 +2,8 @@ alias ..="cd .."
 alias ip="ifconfig"
 alias src="source ~/.zshrc"
 alias nginx.restart="sudo nginx -s stop && sudo nginx"
+alias npmls="npm ls --depth=0"
+alias b="bower"
 
 # alias vhosts="cd /usr/local/etc/nginx/sites-enabled"
 vhosts=/usr/local/etc/nginx/sites-enabled
@@ -9,8 +11,6 @@ vhosts=/usr/local/etc/nginx/sites-enabled
 alias toshop="open -a Adobe\ Photoshop\ CC"
 alias mou="open -a /Applications/Mou.app"
 alias fire="open -a /Applications/Firefox.app"
-
-alias b="bower"
 
 # load other (private) aliases
 source ~/.aliasesrc
